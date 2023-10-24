faf8f128f3d5172169f8a2966a3157d5301427ca
JaeYoonAI <minamljy@gmail.com>
2023-10-24 17:09:39 +0900

    mainpage buttons added

--- users/mypage.js
+++ users/mypage.js
@@ -1,1 +1,17 @@
 console.log("자바스크립트 불러옴");
+
+window.onload = async function loadArticles() {
+    const response = await fetch ("http://127.0.0.1:8000/articles/", {method: "GET"})
+
+    response_json = await response.json()
+    console.log(response_json)
+
+    const articles = document.getElementById("articles")
+
+    response_json.forEach(element => {
+        console.log(element.title)
+        const newArticle = document.createElement("div")
+        newArticle.innerText = element.title
+        articles.appendChild(newArticle)
+    })
+}
