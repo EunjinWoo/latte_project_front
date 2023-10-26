1f328d6afb6457d46ad8e65ce582d4159bf7d0e0
JaeYoonAI <minamljy@gmail.com>
2023-10-26 21:36:24 +0900

    pulled signup

--- users/signup.js
+++ users/signup.js
@@ -1,92 +1,115 @@
 window.onload = () => {
-    console.log("로딩되었음");
-
-
-};
-
-
-
-
-async function handleSignup() {
-    const formData = new FormData();
-    formData.append("username", document.getElementById("username").value);
-    formData.append("age", document.getElementById("age").value);
-    formData.append(
-      "profile_img",
-      document.getElementById("profile_img").files[0]
-    );
-    formData.append("password", document.getElementById("password").value);
-  
-    //   const username = document.getElementById("username").value;
-    //   const age = document.getElementById("age").value;
-    //   const profile_img = document.getElementById("profile_img").files[0];
-    //   const password = document.getElementById("password").value;
-    console.log(username, age, profile_img, password);
-  
-    const response = await fetch("http://127.0.0.1:8000/users/signup/", {
-      headers: {},
-      method: "POST",
-      body: formData,
-    });
-  
-    if (response.status === 201) {
-      alert("회원가입 완료");
-      window.location.href = "http://127.0.0.1:5500";
-    } else {
-      alert("회원가입 실패");
-    }
-  
-    console.log(response);
-}
-  
+  console.log("로딩되었음");
+};  
 
 document.addEventListener('DOMContentLoaded', function () {
-  const imageInput = document.getElementById('imageInput');
-  const ageInput = document.getElementById('ageInput');
-  const editButton = document.getElementById('editButton');
-  const resultImage = document.getElementById('resultImage');
-  const downloadLink = document.getElementById('downloadLink');
+const imageInput = document.getElementById('imageInput');
+const ageInput = document.getElementById('ageInput');
+const editButton = document.getElementById('editButton');
+const signupButton = document.getElementById('signupButton');
+const downloadLink = document.getElementById('downloadLink');
 
-  editButton.addEventListener('click', async function () {
-    const imageFile = imageInput.files[0];
-    const age = ageInput.value;
-    
-    if (!imageFile || !age) {
-        alert('이미지와 나이를 입력하세요.');
-        return;
-    }
+var file = '';
+var changed_image_url = '';
 
-    const formData = new FormData();
-    formData.append('image', imageFile);
-    formData.append('action_type', 'V2_AGE');
-    formData.append('quality_control', 'NONE');
-    formData.append('target', age);
+// 얼굴 특성 편집 -> changed_image_url을 얻음.
+editButton.addEventListener('click', async function () {
+  const imageFile = imageInput.files[0];
+  const age = ageInput.value;
+  
+  if (!imageFile || !age) {
+      alert('이미지와 나이를 입력하세요.');
+      return;
+  }
 
-    try {
-        const response = await fetch('https://www.ailabapi.com/api/portrait/effects/face-attribute-editing', {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'ailabapi-api-key': 'O4FWH470wlnyLX5QMEeIuYs1sV02JOjfAmt6AzHkDNbidb9SWPIBBK9DRTf8LGuc', // 여기에 본인의 API 키를 넣으세요
-            },
-        });
+  const formData = new FormData();
+  formData.append('image_route', imageFile);
+  formData.append('target', age);
 
-        if (response.ok) {
-            const data = await response.json();
-            if (data.error_code === 0) {
-                const imageBase64 = data["result"]["image"];
-                resultImage.src = 'data:image/jpeg;base64,' + imageBase64;
-                downloadLink.href = 'data:image/jpeg;base64,' + imageBase64;
-                downloadLink.download = 'edited_image.jpg';
-                downloadLink.style.display = 'block';
-            } else {
-                alert('얼굴 특성 편집에 실패했습니다: ' + data.error_msg);
-            }
-        } else {
-            alert('서버에서 오류가 발생했습니다.');
-        }
-    } catch (error) {
-        alert('오류 발생: ' + error);
-    }
-  })
+  // DB의 Temp_Profile_Image에 변환된 프로필 이미지 저장. (이 url이 changed_image_url)
+  const response = await fetch("http://127.0.0.1:8000/users/change_profile_image/", {
+    method: "POST",
+    body: formData,
+  });
+
+  const response_json = await response.json()
+
+  if (response.status === 200){
+    // 변환된 결과 이미지 화면에 출력
+    document.getElementById("resultImage").src = `http://127.0.0.1:8000${response_json['changed_image_url']}`;
+
+    console.log(response_json)
+
+    downloadLink.href = 'data:image/jpeg;base64,' + response_json['image_data'];
+    downloadLink.download = 'edited_image.jpg';
+    downloadLink.style.display = 'block';
+
+    changed_image_url = response_json['changed_image_url'];
+  } else { 
+    document.getElementById("imageChangeError").style.opacity = "100%";
+    document.getElementById("imageChangeError").innerText = response_json['error_msg'].split("-").slice(-1);
+  }
+
+})
+
+signupButton.addEventListener('click', async function () {
+  if (changed_image_url === ''){
+    // 아직 프로필 이미지 안 바꾼 경우 다른 나이로.
+    document.getElementById("signupFailedError").style.opacity = "100%";
+    document.getElementById("signupFailedError").innerText = "프로필 이미지 변환은 필수 사항입니다.";
+  }
+  else { // 프로필 이미지 나이 바꿔서 돌린 경우. 
+    fetch(`http://127.0.0.1:8000${changed_image_url}`) // 이미지의 URL을 지정하세요.
+    .then(response => response.blob())
+    .then(blob => {
+      // Blob 객체를 얻었습니다.
+      // 여기서 blob을 파일로 변환할 수 있습니다.
+
+      file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+
+      // 이미지 변환 안 한 경우 아예 회원 가입 버튼을 disable해야할 듯.
+      handleSignup();
+
+      // 이제 'file'은 JavaScript에서 파일 객체로 사용할 수 있음.
+    })
+    .catch(error => {
+      console.error('이미지 가져오기 실패:', error);
+    });
+  }
+})
+
+async function handleSignup() {
+  const formData = new FormData();
+  formData.append("username", document.getElementById("username").value);
+  formData.append("age", document.getElementById("age").value);
+  formData.append("profile_img", file)
+  formData.append("password", document.getElementById("password").value);
+
+  const response = await fetch("http://127.0.0.1:8000/users/signup/", {
+    headers: {},
+    method: "POST",
+    body: formData,
+  });
+
+  if (response.status === 201) {
+    alert("회원가입 완료");
+    window.location.href = "http://127.0.0.1:5500";
+  } else {
+    document.getElementById("signupFailedError").style.opacity = "100%";
+    document.getElementById("signupFailedError").innerText = "회원가입 실패";
+  }
+}
+
 });
+
+async function handleputImagePreview(input) {
+console.log(input.files)
+if (input.files && input.files.length > 0) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        document.getElementById("putImage").src = e.target.result;
+        // console.log(e.target.result)
+    };
+    reader.readAsDataURL(input.files[0]);
+}
+}
